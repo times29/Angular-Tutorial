a253ef4f3cbfe1fe3ed06db451a677822790484f
Timo Schlatter <tds29@posteo.net>
2026-03-19 19:11:53 +0100

    Create home component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: ` <h1>Hello world!</h1> `,
+  imports: [Home],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/public/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.css'],
 })
 export class App {
