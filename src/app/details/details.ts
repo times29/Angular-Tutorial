8b9300b0fee031cb578bcc50de8cd2d237375ee6
Timo Schlatter <tds29@posteo.net>
2026-03-19 19:55:53 +0100

    Display Yes / No instead of true / false

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -23,8 +23,15 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
         <h2 class="section-heading">About this housing location</h2>
         <ul>
           <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+          <li>Does this location have wifi: {{ housingLocation?.wifi ? 'Yes' : 'No' }}</li>
+          <li>
+            Does this location have laundry:
+            @if (housingLocation?.laundry) {
+              Yes
+            } @else {
+              No
+            }
+          </li>
         </ul>
       </section>
       <section class="listing-apply">
