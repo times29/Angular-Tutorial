b5fffc18d4b5bcb41e1edc645b5663595da17467
Timo Schlatter <tds29@posteo.net>
2026-03-19 19:31:42 +0100

    Customize the details page

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -1,5 +1,6 @@
 import { Component, input } from '@angular/core';
 import { HousingLocationInfo } from '../housinglocation';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
@@ -13,9 +14,11 @@ import { HousingLocationInfo } from '../housinglocation';
       />
       <h2 class="listing-heading">{{ housingLocation().name }}</h2>
       <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+      <a [routerLink]="['/details', housingLocation().id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.css'],
+  imports: [RouterLink],
 })
 export class HousingLocation {
   housingLocation = input.required<HousingLocationInfo>();
