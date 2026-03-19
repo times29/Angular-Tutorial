65d717432760062d1d33a20e09a09d55e6a5e049
Timo Schlatter <tds29@posteo.net>
2026-03-19 19:06:25 +0100

    Hello world!

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
-  template: ` <h1>Default</h1> `,
+  template: ` <h1>Hello world!</h1> `,
   styleUrls: ['./app.css'],
 })
 export class App {
-  title = 'default';
+  title = 'homes';
 }
