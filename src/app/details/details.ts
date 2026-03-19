e0e17e01076c260f27468173cac70d85bfaf4a83
Timo Schlatter <tds29@posteo.net>
2026-03-19 19:28:08 +0100

    Add routing

---
+++ src/app/details/details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  template: ` <p>details works!</p> `,
+  styleUrl: './details.css',
+})
+export class Details {}
